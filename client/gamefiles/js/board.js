b4a5f74980b748f447d6ae0097d360154ce29f55
artfoundry <dave@artfoundry.com>
2019-11-27 00:56:31 -0800

    Adding player list and game name to game board

--- client/gamefiles/js/board.js
+++ client/gamefiles/js/board.js
@@ -5,7 +5,11 @@ class Board {
 
     updateBoard(gameData) {
         let $boardContent = $('#game-board-content');
+        let $players = $('#players');
 
-        $boardContent.text(gameData);
+        gameData.playerIds.forEach((name) => {
+            $players.append(`<span class="player-box">${name}</span>`);
+        });
+        $boardContent.append(`<div class="board-game-title">Game: ${gameData.name}</div>`);
     }
 }
